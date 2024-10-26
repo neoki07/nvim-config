0f275cc60da2f43d4de18489678d49f3a6a9c565
neoki <ot07neoki@gmail.com>
2024-10-26 11:08:57 +0900

    feat: update config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("", "H", "^", opts)
+keymap.set("", "L", "$", opts)
+
+keymap.set("", "<C-h>", "^", opts)
+keymap.set("", "<C-l>", "$", opts)
+
+keymap.set("", "<C-j>", "<C-d>", opts)
+keymap.set("", "<C-k>", "<C-u>", opts)
+
+keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
